138316015788322019d7fee897e66b68fc317922
Anu Shibin Joseph Raj <anushibin007@gmail.com>
2023-10-13 23:31:36 +0530

    ✨ Content Storage Service Spec & Build

--- backend/generateandinstallclient.cmd
+++ backend/generateandinstallclient.cmd
@@ -4,11 +4,17 @@ call openapi-generator-cli generate --input-spec ./ot2-apispecs/riskguard.json -
 
 call openapi-generator-cli generate --input-spec ./ot2-apispecs/corecapture.json --generator-name java --package-name com.ot2.corecapture --invoker-package com.ot2.corecapture.invoker --api-package com.ot2.corecapture.api --model-package com.ot2.corecapture.model --group-id com.ot2.corecapture --artifact-id ot2-corecapture-client --output ./ot2-corecapture-client
 
+call openapi-generator-cli generate --input-spec ./ot2-apispecs/contentstorageservice.json --generator-name java --package-name com.ot2.contentstorageservice --invoker-package com.ot2.contentstorageservice.invoker --api-package com.ot2.contentstorageservice.api --model-package com.ot2.contentstorageservice.model --group-id com.ot2.riskguard --artifact-id ot2-contentstorageservice-client --output ./ot2-contentstorageservice-client --global-property skipFormModel=false
+
 cd ot2-riskguard-client
 call mvn clean install
 cd ..
 
 cd ot2-corecapture-client
+call mvn clean install
+cd ..
+
+cd ot2-contentstorageservice-client
 call mvn clean install
 cd ..
 
