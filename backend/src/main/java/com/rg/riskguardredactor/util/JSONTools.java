fda3468107d68cdd4bfc0e6d2c9cea60a1219ea1
Anu Shibin Joseph Raj <ajosephr@opentext.com>
2023-10-26 00:10:44 +0530

    🎨 JSON Parser improvement

--- backend/src/main/java/com/rg/riskguardredactor/util/JSONTools.java
+++ backend/src/main/java/com/rg/riskguardredactor/util/JSONTools.java
@@ -1,5 +1,6 @@
 package com.rg.riskguardredactor.util;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class JSONTools {
@@ -7,6 +8,10 @@ public class JSONTools {
 	private static final ObjectMapper objectMapper = new ObjectMapper();
 
 	public static ObjectMapper getObjectMapper() {
+		// Fix for add Module "com.fasterxml.jackson.datatype:jackson-datatype-jsr310"
+		// to enable handling
+		objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+		objectMapper.findAndRegisterModules();
 		return objectMapper;
 	}
 }
