27807ad3e49b45a4947f16ec217a06a5d78802c5
Anu Shibin Joseph Raj <ajosephr@opentext.com>
2023-10-28 17:26:28 +0530

    ⚰ Don't call the openapi generator every time

--- backend/generateandinstallclient.cmd
+++ backend/generateandinstallclient.cmd
@@ -1,10 +1,10 @@
 rem npm install @openapitools/openapi-generator-cli -g
 
-call openapi-generator-cli generate --input-spec ./ot2-apispecs/riskguard.json --generator-name java --package-name com.ot2.riskguard --invoker-package com.ot2.riskguard.invoker --api-package com.ot2.riskguard.api --model-package com.ot2.riskguard.model --group-id com.ot2.riskguard --artifact-id ot2-riskguard-client --output ./ot2-riskguard-client
+rem call openapi-generator-cli generate --input-spec ./ot2-apispecs/riskguard.json --generator-name java --package-name com.ot2.riskguard --invoker-package com.ot2.riskguard.invoker --api-package com.ot2.riskguard.api --model-package com.ot2.riskguard.model --group-id com.ot2.riskguard --artifact-id ot2-riskguard-client --output ./ot2-riskguard-client
 
-call openapi-generator-cli generate --input-spec ./ot2-apispecs/corecapture.json --generator-name java --package-name com.ot2.corecapture --invoker-package com.ot2.corecapture.invoker --api-package com.ot2.corecapture.api --model-package com.ot2.corecapture.model --group-id com.ot2.corecapture --artifact-id ot2-corecapture-client --output ./ot2-corecapture-client
+rem call openapi-generator-cli generate --input-spec ./ot2-apispecs/corecapture.json --generator-name java --package-name com.ot2.corecapture --invoker-package com.ot2.corecapture.invoker --api-package com.ot2.corecapture.api --model-package com.ot2.corecapture.model --group-id com.ot2.corecapture --artifact-id ot2-corecapture-client --output ./ot2-corecapture-client
 
-call openapi-generator-cli generate --input-spec ./ot2-apispecs/contentstorageservice.json --generator-name java --package-name com.ot2.contentstorageservice --invoker-package com.ot2.contentstorageservice.invoker --api-package com.ot2.contentstorageservice.api --model-package com.ot2.contentstorageservice.model --group-id com.ot2.riskguard --artifact-id ot2-contentstorageservice-client --output ./ot2-contentstorageservice-client --global-property skipFormModel=false
+rem call openapi-generator-cli generate --input-spec ./ot2-apispecs/contentstorageservice.json --generator-name java --package-name com.ot2.contentstorageservice --invoker-package com.ot2.contentstorageservice.invoker --api-package com.ot2.contentstorageservice.api --model-package com.ot2.contentstorageservice.model --group-id com.ot2.riskguard --artifact-id ot2-contentstorageservice-client --output ./ot2-contentstorageservice-client --global-property skipFormModel=false
 
 cd ot2-riskguard-client
 call mvn clean install
