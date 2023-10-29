230ad453d6af04417cb7b655f413613e3022c92e
Anu Shibin Joseph Raj <ajosephr@opentext.com>
2023-10-30 01:49:07 +0530

    🐛 New updates were overriding old ones

--- backend/src/main/java/com/rg/riskguardredactor/service/db/DocumentStateService.java
+++ backend/src/main/java/com/rg/riskguardredactor/service/db/DocumentStateService.java
@@ -1,5 +1,7 @@
 package com.rg.riskguardredactor.service.db;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,18 +16,31 @@ public class DocumentStateService {
 	DocumentStateRepo repo;
 
 	public DocumentState setRedactedUrl(String id, String redactedDocUrl) {
-		DocumentState state = new DocumentState();
-		state.setId(id);
+		DocumentState state = getDocumentState(id);
 		state.setRedactedDocUrl(redactedDocUrl);
 		repo.save(state);
 		return state;
 	}
 
 	public DocumentState setCoreCaptureStatus(String id, ProcessStatusModel coreCaptureStatus) {
-		DocumentState state = new DocumentState();
-		state.setId(id);
+		DocumentState state = getDocumentState(id);
 		state.setCoreCaptureStatus(coreCaptureStatus);
 		repo.save(state);
+		return state;
+	}
+
+	private DocumentState getDocumentState(String id) {
+		// Find current item in the DB
+		Optional<DocumentState> findById = repo.findById(id);
+
+		// If we found something, simply return it
+		if (findById.isPresent()) {
+			return findById.get();
+		}
+
+		// If we didn't, create a new one and return it
+		DocumentState state = new DocumentState();
+		state.setId(id);
 		return state;
 	}
 }
